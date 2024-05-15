9699e1623a7a748915c713d1b761cff664b52ce1
ChuCong <2conglc.vn@gmail.com>
2024-05-15 10:19:46 +0700

    update

--- SWF Decompiler/Form1.Designer.vb
+++ SWF Decompiler/Form1.Designer.vb
@@ -24,13 +24,46 @@ Partial Class Form1
     Private Sub InitializeComponent()
         SaveFileDialog1 = New SaveFileDialog()
         OpenFileDialog1 = New OpenFileDialog()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(96, 167)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(134, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Compress CWS"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(96, 196)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(134, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "Compress ZWS"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(96, 225)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(134, 23)
+        Button3.TabIndex = 2
+        Button3.Text = "DeCompress"
+        Button3.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
@@ -38,5 +71,8 @@ Partial Class Form1
 
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 
 End Class
--- SWF Decompiler/Form1.vb
+++ SWF Decompiler/Form1.vb
@@ -1,3 +1,67 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            OpenFileDialog1.Filter = "All Files(*.swf)|*.swf"
+            SaveFileDialog1.Filter = "All Files(*.swf)|*.swf"
 
+
+            If OpenFileDialog1.ShowDialog = DialogResult.OK AndAlso SaveFileDialog1.ShowDialog = DialogResult.OK Then
+
+                Dim source As SWF = New SWF(IO.File.ReadAllBytes(OpenFileDialog1.FileName))
+
+                source.Compress(SaveFileDialog1.FileName, SWF.CompressTionTypes.CWS)
+
+
+                MessageBox.Show("ok")
+            End If
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            OpenFileDialog1.Filter = "All Files(*.swf)|*.swf"
+            SaveFileDialog1.Filter = "All Files(*.swf)|*.swf"
+
+
+            If OpenFileDialog1.ShowDialog = DialogResult.OK AndAlso SaveFileDialog1.ShowDialog = DialogResult.OK Then
+
+                Dim source As SWF = New SWF(IO.File.ReadAllBytes(OpenFileDialog1.FileName))
+
+                source.Compress(SaveFileDialog1.FileName, SWF.CompressTionTypes.ZWS)
+
+
+                MessageBox.Show("ok")
+            End If
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            OpenFileDialog1.Filter = "All Files(*.swf)|*.swf"
+            SaveFileDialog1.Filter = "All Files(*.swf)|*.swf"
+
+
+            If OpenFileDialog1.ShowDialog = DialogResult.OK AndAlso SaveFileDialog1.ShowDialog = DialogResult.OK Then
+
+                Dim source As SWF = New SWF(IO.File.ReadAllBytes(OpenFileDialog1.FileName))
+
+                source.DeCompress(SaveFileDialog1.FileName)
+
+
+                MessageBox.Show("ok")
+            End If
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
 End Class
