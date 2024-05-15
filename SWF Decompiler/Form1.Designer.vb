f600eafc315b37d25e2265fb615ecc9b5525c0b7
ChuCong <2conglc.vn@gmail.com>
2024-05-15 10:14:35 +0700

    update

--- SWF Decompiler/Form1.Designer.vb
+++ SWF Decompiler/Form1.Designer.vb
@@ -22,60 +22,21 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        OpenFileDialog1 = New OpenFileDialog()
         SaveFileDialog1 = New SaveFileDialog()
-        Button1 = New Button()
-        Button2 = New Button()
-        Button3 = New Button()
+        OpenFileDialog1 = New OpenFileDialog()
         SuspendLayout()
-        ' 
-        ' Button1
-        ' 
-        Button1.Location = New Point(87, 237)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(143, 23)
-        Button1.TabIndex = 0
-        Button1.Text = "Compress CWS"
-        Button1.UseVisualStyleBackColor = True
-        ' 
-        ' Button2
-        ' 
-        Button2.Location = New Point(87, 266)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(143, 23)
-        Button2.TabIndex = 1
-        Button2.Text = "Compress ZWS"
-        Button2.UseVisualStyleBackColor = True
-        ' 
-        ' Button3
-        ' 
-        Button3.Location = New Point(87, 295)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(143, 23)
-        Button3.TabIndex = 2
-        Button3.Text = "Decompress"
-        Button3.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
-        Controls.Add(Button3)
-        Controls.Add(Button2)
-        Controls.Add(Button1)
-        FormBorderStyle = FormBorderStyle.FixedSingle
-        MaximizeBox = False
         Name = "Form1"
-        StartPosition = FormStartPosition.CenterScreen
         Text = "Form1"
         ResumeLayout(False)
     End Sub
 
-    Friend WithEvents OpenFileDialog1 As OpenFileDialog
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
 
 End Class
