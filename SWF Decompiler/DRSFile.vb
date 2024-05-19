513ea13c6715d40916d849298ec04ffdbbf291e8
ChuCong <61927498+2CongLC@users.noreply.github.com>
2024-05-19 20:56:06 +0700

    Update DRSFile.vb

--- SWF Decompiler/DRSFile.vb
+++ SWF Decompiler/DRSFile.vb
@@ -1,1 +1,24 @@
+Imports System
+Imports System.IO
+Imports System.Text
 
+Public Class DRSFile
+
+Private info as string '40 bytes
+Private version as string '4 bytes
+Private ftype as string '12 bytes
+Private tablecount as integer '4 bytes
+Private foffset as integer '4 bytes
+
+
+
+
+
+
+
+
+
+
+
+
+End Class 
